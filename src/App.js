b8571e8dd72a9857c8f4e256f88e6a7a8d05ad55
webpawan <pawansaini66477@gmail.com>
2023-01-13 23:31:56 +0530

    basic ui is complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+
+
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <div className="main"></div>
+        <div className="pyramid">
+          <ul className="moneyList">
+            <li className="moneyListItem active">
+              <span className="moneyListItemNumber">4</span>
+              <span className="moneyListItemAmount">400</span>
+            </li>
+            <li className="moneyListItem">
+              <span className="moneyListItemNumber">4</span>
+              <span className="moneyListItemAmount">400</span>
+            </li>
+            <li className="moneyListItem">
+              <span className="moneyListItemNumber">4</span>
+              <span className="moneyListItemAmount">400</span>
+            </li>
+            <li className="moneyListItem">
+              <span className="moneyListItemNumber">4</span>
+              <span className="moneyListItemAmount">400</span>
+            </li>
+            <li className="moneyListItem">
+              <span className="moneyListItemNumber">4</span>
+              <span className="moneyListItemAmount">400</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
